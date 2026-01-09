46915355f9e3aa06d5af97ad0b1eef681f5c5f82
Sherqo <sharqawycs@gmail.com>
2026-01-09 15:25:58 +0200

    add run_dir function

---
+++ app.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+void run_dir(const char *path) {
+  DIR *dir = opendir(path);
+  if (!dir) {
+    perror(path);
+    return;
+  }
+
+  struct dirent *entry;
+  while ((entry = readdir(dir)) != NULL) {
+    if (!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, "..")) continue; // Aborted (core dumped) :)
+
+    char *fullpath = malloc(strlen(path) + strlen(entry->d_name) + 2);
+    sprintf(fullpath, "%s/%s", path, entry->d_name);
+
+    struct stat st;
+    if (stat(fullpath, &st) == -1) {
+      perror(fullpath);
+      continue;
+    }
+
+    if (S_ISDIR(st.st_mode)) {
+      printf("%s\n", fullpath);
+      run_dir(fullpath); // i do not like recursion, TODO: fix
+    } else if (S_ISREG(st.st_mode)) {
+      printf("%s\n", fullpath);
+      // count_file_lines(fullpath);
+    }
+
+    free(fullpath);
+  }
+
+  closedir(dir);
+}
+
+int main(int argc, char **argv) {
+  const char *path = (argc > 1) ? argv[1] : ".";
+
+  run_dir(path);
+  return 0;
+}
